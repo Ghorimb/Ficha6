232c910f25d38b0f1ae0de3df0b1eea31118b2b3
Ghorimb <bamorim17@gmail.com>
2023-10-16 15:54:33 +0100

    Exercício 3 e 6, ficha 6

---
+++ src/test/java/DigitosTeste.java
@@ -0,0 +1,27 @@
+import org.example.Digitos;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DigitosTeste {
+    private Digitos digitos;
+
+    @BeforeEach
+    public void setUp() {
+        digitos = new Digitos();
+    }
+
+    @Test
+    public void testCalcularMediaDigitosNumeroUnicoDigito() {
+        double media = Digitos.calcularMediaDigitos(7);
+        assertEquals(7.0, media, 0.001);
+    }
+
+    @Test
+    public void testCalcularMediaDigitosNumeroPositivo() {
+        double media = Digitos.calcularMediaDigitos(12345);
+        assertEquals(3.0, media, 0.001);
+    }
+
+}
